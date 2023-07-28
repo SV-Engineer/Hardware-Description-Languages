ee6732a6ca07004ef5f55ea0ac75b4abad638f40
Austin <systemverilogengineer@gmail.com>
2023-07-28 16:22:48 -0500

    Added some base skeletons and a package definition example, untested

---
+++ System-Verilog/Synthesizable/pkg/syntax_pkg.sv
@@ -0,0 +1,34 @@
+//####################################################################
+//  Author: Austin | @SV-Engineer
+//    Date: 28JUL2023
+// Purpose: This file is for demonstrating the syntax for a package.
+//####################################################################
+
+package demonstration;
+
+  logic someBit;
+  logic [1:0] someBitField;
+
+  const struct packed
+  {
+    logic [15:0] a;
+    logic [15:0] b;
+  } genericConstStruct;
+
+  typedef union packed
+  {
+    logic [31:0] data;
+
+    struct packed
+    {
+      logic [7:0] byte3;
+      logic [7:0] byte2;
+      logic [7:0] byte1;
+      logic [7:0] byte0;
+    } bytes;
+
+  } genericUnion_t;
+
+endpackage : demonstration
+
+
